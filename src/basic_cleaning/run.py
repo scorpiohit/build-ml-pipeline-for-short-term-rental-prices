51d8c95f0b91c2f9796c759aeb07ee5cc3ec7dec
git <khtm671@IEDBSHARMMO.alxn.net>
2022-12-17 20:33:57 +0000

    1. Created basic_cleaning folder using cookie-cutter 2. Updated MLproject, run.py and conda.yml files in basic_cleaning folder as per specifications provided

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Performs basic cleaning on the data and save the results in Weights & Biases
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
 """
 import argparse
 import logging
@@ -24,35 +24,87 @@ def go(args):
     # YOUR CODE HERE     #
     ######################
 
+    logger.info("Downloading artifact")
+    artifact = run.use_artifact(args.input_artifact)
+    artifact_path = artifact.file()
+
+    # Read artifact
+    data_frame = pd.read_csv(artifact_path)
+
+    # Drop outliers
+    logger.info("Dropping outlier")
+    min_price = args.min_price
+    max_price = args.max_price
+    idx = data_frame['price'].between(min_price, max_price)
+    data_frame = data_frame[idx].copy()
+
+    # Convert last_review to datetime
+    logger.info("Convert last_review columns to date")
+    data_frame['last_review'] = pd.to_datetime(data_frame['last_review'])
+
+    # Drop Outlier from longitude and latitude
+    idx = data_frame['longitude'].between(-74.25, - \
+                                          73.50) & data_frame['latitude'].between(40.5, 41.2)
+    data_frame = data_frame[idx].copy()
+
+    # Saving the artifact
+    filename = args.output_artifact
+    data_frame.to_csv(filename, index=False)
+
+    artifact = wandb.Artifact(
+        name=args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file(filename)
+
+    logger.info("Logging artifact")
+    run.log_artifact(artifact)
+
+    os.remove(filename)
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="This steps cleans the data")
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
 
 
     parser.add_argument(
-        "--parameters [parameter1", 
+        "--input_artifact", 
         type=## INSERT TYPE HERE: str, float or int,
         help=## INSERT DESCRIPTION HERE,
         required=True
     )
 
     parser.add_argument(
-        "--parameter2]: parameter1", 
+        "--output_artifact", 
         type=## INSERT TYPE HERE: str, float or int,
         help=## INSERT DESCRIPTION HERE,
         required=True
     )
 
     parser.add_argument(
-        "--parameter2", 
+        "--output_type", 
         type=## INSERT TYPE HERE: str, float or int,
         help=## INSERT DESCRIPTION HERE,
         required=True
     )
 
     parser.add_argument(
-        "--parameter3", 
+        "--output_description", 
+        type=## INSERT TYPE HERE: str, float or int,
+        help=## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price", 
+        type=## INSERT TYPE HERE: str, float or int,
+        help=## INSERT DESCRIPTION HERE,
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price", 
         type=## INSERT TYPE HERE: str, float or int,
         help=## INSERT DESCRIPTION HERE,
         required=True
