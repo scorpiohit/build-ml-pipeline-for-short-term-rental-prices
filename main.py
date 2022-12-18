408857e3576be0c6013356389d2db9dea642aba4
git <khtm671@IEDBSHARMMO.alxn.net>
2022-12-18 00:59:11 +0000

    1. Added version="main" on the data_split section in main.py

--- main.py
+++ main.py
@@ -88,8 +88,9 @@ def go(config: DictConfig):
             # Implement here #
             ##################
             _ = mlflow.run(
-                f"{config['main']['components']}/train_val_test_split",
+                f"{config['main']['components_repository']}/train_val_test_split",
                 "main",
+                version="main",
                 parameters={
                     "input": "clean_sample.csv:latest",
                     "test_size": config["modeling"]["test_size"],
