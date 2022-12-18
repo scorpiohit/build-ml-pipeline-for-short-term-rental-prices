a14abfc67f2ca863b951e1178bd0790d60cbca66
git <khtm671@IEDBSHARMMO.alxn.net>
2022-12-18 00:35:37 +0000

    1. Missed pushing main.py from previous commit, so commiting now

--- main.py
+++ main.py
@@ -88,7 +88,15 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify": config["modeling"]["stratify_by"]},
+            )
 
         if "train_random_forest" in active_steps:
 
